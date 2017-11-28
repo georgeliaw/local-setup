366781778c31564ae55e94fe767b1368a4a44ce3
George Liaw <george.a.liaw@gmail.com>
2017-11-27 23:57:15 -0800

    update scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# TODO add ansible install here
+# Install Pip
+if [ ! -f /usr/local/bin/pip ]; then
+    cd ~/Downloads
+    wget https://bootstrap.pypa.io/get-pip.py
+    sudo python get-pip.py
+fi
 
+# Install ansible
+if [ ! -f ~/.local/bin/ansible ]; then
+    pip install ansible --user
+fi
+
+# Run playbook
 ansible-playbook setup.yml --ask-become-pass
--- ubuntu.sh
+++ ubuntu.sh
@@ -7,7 +7,6 @@ mkdir -p ~/Projects
 BAZEL_VERSION=0.5.2
 
 # Add PPAs
-sudo apt-add-repository -y ppa:ansible/ansible
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:webupd8team/terminix
 sudo add-apt-repository -y ppa:snwh/pulp
