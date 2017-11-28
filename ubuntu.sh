98eebbb33ed4a1e1a18e210b2ca971766db31dbd
George Liaw <george.a.liaw@gmail.com>
2017-11-27 16:21:51 -0800

    update script to auto-accept

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,29 +7,29 @@ CURRENT_DIR=${pwd}
 BAZEL_VERSION=0.5.2
 
 # Add PPAs
-sudo add-apt-repository ppa:webupd8team/java
-sudo add-apt-repository ppa:webupd8team/terminix
-sudo add-apt-repository ppa:snwh/pulp
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo add-apt-repository -y ppa:webupd8team/terminix
+sudo add-apt-repository -y ppa:snwh/pulp
 sudo apt-get update
 
 # Install base packages
-sudo apt-get install pkg-config zip g++ zlib1g-dev unzip
-sudo apt-get install gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0
+sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip
+sudo apt-get install -y gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0
 
 # Install Java 8
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get install oracle-java8-installer
+sudo apt-get install -y oracle-java8-installer
 
 # Install Gnome3 Desktop
-sudo apt-get install gnome-shell
+sudo apt-get install -y gnome-shell
 
 # Install Tilix
-sudo apt-get install tilix
+sudo apt-get install -y tilix
 
 # Install Paper themes
-sudo apt-get install paper-icon-theme
-sudo apt-get install paper-cursor-theme
-sudo apt-get install paper-gtk-theme
+sudo apt-get install -y paper-icon-theme
+sudo apt-get install -y paper-cursor-theme
+sudo apt-get install -y paper-gtk-theme
 
 # Install Bazel
 wget https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
@@ -44,7 +44,7 @@ cp bazel-bin/scripts/bazel-complete.bash ~/.bazel/bin/
 cd ${CURRENT_DIR}
 
 # Install Silver Searcher
-apt-get install silversearcher-ag
+apt-get install -y silversearcher-ag
 
 # Install Fuzzy Finder
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
