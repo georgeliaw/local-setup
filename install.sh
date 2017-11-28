fc2242af2d73b1eba070aa225b441fad5f6a7fd0
George Liaw <george.a.liaw@gmail.com>
2017-11-28 01:30:19 -0800

    fix typos

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
+set -ex
 
 # Install Pip
-if [ ! -f /usr/local/bin/pip ]; then
+if [[ ! -f /usr/local/bin/pip ]]; then
     cd ~/Downloads
     wget https://bootstrap.pypa.io/get-pip.py
     sudo python get-pip.py
 fi
 
 # Install ansible
-if [ ! -f ~/.local/bin/ansible ]; then
+if [[ ! -f ~/.local/bin/ansible ]]; then
     pip install ansible --user
     echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 fi
 
 # Update bashrc
-if [ -z $(grep '^export PATH="$PATH:$HOME/.local/bin"$' ~/.bashrc) ]; then
+if [[ -z $(grep '^export PATH="$PATH:$HOME/.local/bin"$' ~/.bashrc) ]]; then
     echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 fi
 
