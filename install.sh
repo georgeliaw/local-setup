669241dd7e0320db916bc6baa92d1645f15340c6
George Liaw <george@6sense.com>
2019-10-14 21:05:26 -0700

    minor edits

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -ex
+REPO_DIR=$PWD
 
 # Install Python3.7
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt-get install python3.7 -y
+sudo apt-get install python2.7 python3.7 -y
 
 # Install Pip
 if [[ ! -f /usr/local/bin/pip ]]; then
     cd ~/Downloads
     wget https://bootstrap.pypa.io/get-pip.py
-    sudo python3.7 get-pip.py
+    sudo python get-pip.py
+    cd $REPO_DIR
 fi
 
 # Install ansible
@@ -25,6 +27,6 @@ if [[ -z $(grep '^export PATH="$PATH:$HOME/.local/bin"$' ~/.bashrc) ]]; then
 fi
 
 # Run playbook
-ansible-playbook setup.yml --ask-become-pass
+~/.local/bin/ansible-playbook setup.yml --ask-become-pass
 
 source ~/.bashrc
